ec39afb8c6d854bf4c3a4b1b06f9cc9a5694427b
Mohit Virli <virlimohit@gmail.com>
2025-02-19 00:56:36 +0530

    Added noise overlay

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,6 +16,12 @@ const Hero = () => {
     gsap.fromTo('.base-canvas', { opacity: 0 }, { opacity: 1, duration: 2, delay: 0.5 });
   }, []);
 
+  const noiseOverlayStyle = {
+    backgroundBlendMode: "overlay",
+    backgroundImage: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 600 600'%3E%3Cfilter id='a'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23a)'/%3E%3C/svg%3E\")",
+    backgroundRepeat: "repeat",
+    backgroundSize: "182px",
+  }
   const fontProps = {
     font: "./soria-font.ttf",
     fontSize: 1.2,
@@ -23,7 +29,7 @@ const Hero = () => {
   }
   return (
     <div className="min-h-screen bg-sky-600 relative">
-      <Canvas className="w-full h-full base-canvas"
+      <Canvas className="w-full h-full base-canvas bg-sky-600"
         style={{
           position: "absolute",
           top: 0,
@@ -31,6 +37,7 @@ const Hero = () => {
           left: 0,
           right: 0,
           overflow: "hidden",
+          ...noiseOverlayStyle,
         }}
         >
           <Suspense fallback={null}>
--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,7 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-  <div className="min-h-screen relative">
-    <div
-      style={{
-        backgroundColor: "transparent",
-        backgroundImage: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 600 600'%3E%3Cfilter id='a'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23a)'/%3E%3C/svg%3E\")",
-        backgroundRepeat: "repeat",
-        backgroundSize: "182px",
-        opacity: 0.7,
-        top: 0,
-        left: 0,
-        position: "absolute",
-        width: "100%",
-        height: "100%"
-      }}>
-    </div>
+    <div className="min-h-screen relative">
       <Hero />
     </div>
   );
