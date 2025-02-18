585b5b545053cce116b92bd24bc5e35258541040
Mohit Virli <virlimohit@gmail.com>
2025-02-18 22:15:17 +0530

    Fix: Cloud positions

--- app/components/Cloud.tsx
+++ app/components/Cloud.tsx
@@ -11,6 +11,7 @@ const CloudContainer = () => {
 
 
     state.camera.rotation.x = -0.5 * Math.PI * a;
+
     if (b < 0.5) {
       state.camera.position.y = - 30 * b;
     } else if (b < 1){
@@ -21,18 +22,82 @@ const CloudContainer = () => {
   });
 
   return (
-    <Clouds material={THREE.MeshBasicMaterial}>
-      <Cloud
-        seed={11}
-        segments={6}
-        concentrate={'inside'}
+    <Clouds material={THREE.MeshBasicMaterial} position={[0, -3.5, 0]} frustumCulled={false}>
+      <Cloud seed={1}
+        segments={1}
+        concentrate="inside"
         bounds={[10, 10, 10]}
+        growth={3}
+        position={[-1, 0, 0]}
+        smallestVolume={2}
+        scale={1.9}
+        volume={2}
+        speed={0.2}
+        fade={5}
+        />
+      <Cloud
+        seed={3}
+        segments={1}
+        concentrate="outside"
+        bounds={[10, 10, 10]}
+        growth={2}
+        position={[2, 0, 2]}
+        smallestVolume={2}
+        scale={1}
+        volume={2}
+        fade={3}
+        speed={0.1}/>
+
+      <Cloud
+        seed={4}
+        segments={1}
+        concentrate="outside"
+        bounds={[10, 20, 15]}
         growth={4}
-        smallestVolume={4}
+        position={[-10, -10, 4]}
+        smallestVolume={2}
+        scale={2}
+        speed={0.2}
+        volume={3}/>
+
+      <Cloud
+        seed={5}
+        segments={1}
+        concentrate="outside"
+        bounds={[5, 5, 5]}
+        growth={2}
+        position={[6, -3, 8]}
+        smallestVolume={2}
         scale={2}
         volume={2}
-        fade={10}
-        speed={0.2}/>
+        fade={0.1}
+        speed={0.1}/>
+
+      <Cloud
+        seed={6}
+        segments={1}
+        concentrate="outside"
+        bounds={[5, 5, 5]}
+        growth={2}
+        position={[0, -15, 20]}
+        smallestVolume={2}
+        scale={4}
+        volume={3}
+        fade={0.1}
+        speed={0.1}/>
+
+      <Cloud
+        seed={7}
+        segments={1}
+        concentrate="outside"
+        bounds={[5, 5, 5]}
+        growth={2}
+        position={[10, -20, -10]}
+        smallestVolume={2}
+        scale={4}
+        volume={3}
+        fade={0.1}
+        speed={0.1}/>
     </Clouds>);
 }
 
