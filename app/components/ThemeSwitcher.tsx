04692f3c083481e8753b684c143a0f8a5bf2eb00
Mohit Virli <virlimohit@gmail.com>
2025-04-19 16:21:42 +0530

    Handle Work Portal section

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -1,26 +1,27 @@
 // import "react-crud-icons/dist/react-crud-icons.css";
 
 import { useGSAP } from "@gsap/react";
-import Image from 'next/image';
-import { useThemeStore } from "@stores";
-import { useRef } from "react";
+import { usePortalStore, useThemeStore } from "@stores";
 import gsap from "gsap";
+import Image from 'next/image';
+import { useRef } from "react";
 
 const ThemeSwitcher = () => {
   const themeSwitcherRef = useRef<HTMLDivElement>(null);
   const nextColor = useThemeStore((state) => state.nextColor);
+  const isActive = usePortalStore((state) => state.activePortalId);
   const toggleTheme = () => nextColor();
 
   useGSAP(() => {
     gsap.to(themeSwitcherRef.current, {
-      opacity: 1,
-      duration: 2,
-      delay: 0.5,
+      opacity: isActive ? 0 : 1,
+      duration: 1,
+      delay: isActive ? 0 : 1,
     });
-  }, []);
+  }, [isActive]);
 
   return (
-    <div className="fixed right-5 bottom-5" ref={themeSwitcherRef} style={{ opacity: 0 }}>
+    <div className="fixed left-5 bottom-5" ref={themeSwitcherRef} style={{ opacity: 0, zIndex: 2 }}>
       <div className="flex items-center justify-center gap-2">
         <a className="hover:cursor-pointer" onClick={toggleTheme}>
           <Image src="/night-mode.svg" width={24} height={24} alt="night mode" loading="lazy" />
