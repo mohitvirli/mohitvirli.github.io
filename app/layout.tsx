67ff263b2f984836f5f898b666d78c202e401529
Mohit Virli <virlimohit@gmail.com>
2025-05-04 21:20:09 +0530

    Setup SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,41 @@ const vercettiFont = localFont({
 export const metadata: Metadata = {
   title: "Mohit Virli ✌️",
   description: "A frontend developer by profession, a creative at heart.",
+  keywords: "Mohit Virli, Frontend Engineer, React Developer, Three.js, Creative Developer, Web Development, Angular, JavaScript, TypeScript, Portfolio",
+  authors: [{ name: "Mohit Virli" }],
+  creator: "Mohit Virli",
+  publisher: "Mohit Virli",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: "Mohit Virli - Frontend Engineer",
+    description: "Frontend engineer by profession, creative at heart.",
+    url: "https://mohitvirli.github.io",
+    siteName: "Mohit Virli's Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mohit Virli - Frontend Engineer",
+    description: "Frontend engineer by profession, creative at heart.",
+  },
+  verification: {
+    google: "GsRYY-ivL0F_VKkfs5KAeToliqz0gCrRAJKKmFkAxBA",
+  },
 };
 
 export default function RootLayout({
